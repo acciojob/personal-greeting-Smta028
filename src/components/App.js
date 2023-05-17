5e0e8ab9e291c4aab620ed8bfa341f3a2e8b6458
smta028 <samtasingh028@gmail.com>
2023-05-17 09:50:00 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [name,setName]=useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>hello</h1>
+        <label htmlFor="name">Enter your name:</label>
+        <br />
+        <input type="text" onChange={(e)=>setName(e.target.value)} />
+        {name&&<h2>Hello {name}!</h2>}
     </div>
+
+  
   )
 }
 
